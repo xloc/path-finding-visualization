aebb13bf2170e6398726a4eabf7a2bd479ba60b0
xloc <xloc.cc@outlook.com>
2021-01-23 00:47:10 -0800

    add case of path-not-found

--- src/DijkstraRouting.js
+++ src/DijkstraRouting.js
@@ -4,7 +4,7 @@ export default class DijkstraRouting {
   constructor(gridModel, sources, targets) {
     const { nRow, nCol } = gridModel;
 
-    targets = new Set(targets.map(({x, y}) => JSON.stringify([x, y])));
+    targets = new Set(targets.map(({ x, y }) => JSON.stringify([x, y])));
 
     this.isBlocked = (i, j) => {
       if (i < 0 || i >= nRow || j < 0 || j >= nCol) return true;
@@ -14,8 +14,7 @@ export default class DijkstraRouting {
     }
 
     this.isConnected = (i, j) => {
-      console.log({i, j});
-      return (targets.has(JSON.stringify([i, j])));
+      return targets.has(JSON.stringify([i, j]));
     }
 
     const processingGrid = Array(nRow).fill(null)
@@ -23,20 +22,39 @@ export default class DijkstraRouting {
         .map((a, y) => 0)
       );
 
-    const expansionList = sources.map(({x, y}) => [x, y])
+    const expansionList = sources.map(({ x, y }) => [x, y])
     this.states = { processingGrid, expansionList, iExpand: 1 }
   }
 
   next() {
-    if (this.states.finished) return;
-    this.states = expand(this.isBlocked, this.isConnected, this.states);
-    return this.states;
+    if (!this.states.expansionFinished)
+      this.states = expand(this.isBlocked, this.isConnected, this.states);
+    else {
+      if (this.states.pathFound) {
+        this.states = backTrace(this.states);
+      }
+    }
   }
 }
 
+function backTrace({ connectedCoor, processingGrid }) {
+
+}
+
 function expand(isBlocked, isConnected, states) {
-  if (states.finished) return states;
-  const { processingGrid, expansionList, iExpand } = states
+  if (states.expansionFinished) return states;
+
+  const { processingGrid, expansionList, iExpand } = states;
+  if (states.expansionList.length === 0) {
+    return {
+      expansionFinished: true,
+      pathFound: false,
+      expansionList: [],
+      processingGrid: processingGrid,
+      iExpand
+    }
+  }
+
 
   const nextExpansionList = new Set();
   for (const [ci, cj] of expansionList) {
@@ -44,20 +62,18 @@ function expand(isBlocked, isConnected, states) {
 
     if (isConnected(ci, cj)) {
       return {
-        finished: true,
+        expansionFinished: true,
+        pathFound: true,
         connectedCoor: [ci, cj],
         expansionList: [],
-        processingGrid: processingGrid, 
+        processingGrid: processingGrid,
         iExpand
       }
     }
 
     for (const [i, j] of [[ci + 1, cj], [ci - 1, cj], [ci, cj + 1], [ci, cj - 1]]) {
-      // console.log({i, j, blocked: isBlocked(i, j), pg: processingGrid[i][j] === 0});
-      // console.log({i, j});
 
       if (!isBlocked(i, j) && processingGrid[i][j] === 0) {
-        // console.log({i, j, blocked: isBlocked(i, j), pg: processingGrid[i][j] === 0});
         const jsonCoors = JSON.stringify([i, j])
         if (!nextExpansionList.has(jsonCoors)) {
           nextExpansionList.add(jsonCoors);
