87297a52a651f11c93c6223af7be64bd9e792112
xloc <xloc.cc@outlook.com>
2021-01-29 23:15:08 -0800

    fetch infile list from the server

--- src/App.tsx
+++ src/App.tsx
@@ -3,15 +3,10 @@ import "./App.css";
 import Grid from "./Components/Grid";
 import { parseRoutingMapString } from "./Models/RouteMap";
 import data from "./routeMapData";
-import {
-  makeObstacleGrid,
-  routeNet,
-  NetRoutingSuccess,
-  route,
-  MapRouteSuccess,
-} from "./Routers/Router";
+import { route, MapRouteSuccess } from "./Routers/Router";
 
 import { Grid as GridModel } from "./Models/Grid";
+import axios from "axios";
 
 export interface RouteResultCell {
   netId: number;
@@ -56,10 +51,20 @@ function App() {
     // setRouteResult(grid);
   }
 
+  const [infiles, setInfiles] = useState();
+  useEffect(() => {
+    axios.get("benchmarks/infiles.json").then((res) => {
+      console.log(res);
+      setInfiles(res.data);
+    });
+    return () => {};
+  }, []);
+
   return (
     <div className="App">
       <Grid routeMap={routeMap} routeResult={routeResult} />
       <button onClick={next}> Next </button>
+      <pre>{JSON.stringify(infiles, null, 2)}</pre>
     </div>
   );
 }
